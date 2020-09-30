afe81b5b954ad35b5b67cbe8ec8068ed4fd79179
AnuchaSu <art0913452643@gmail.com>
2020-09-30 14:17:31 +0700

    typingpage

--- mean/src/app/homepage/homepage.component.ts
+++ mean/src/app/homepage/homepage.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { lorem } from "faker";
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent implements OnInit {
+export class HomepageComponent {
+  randomText = lorem.sentence();
+  enteredText = "";
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onInput(value: string) {
+    this.enteredText = value;
   }
 
+  compares(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return "pending";
+    }
+
+    return randomLetter === enteredLetter ? "correct" : "incorrect";
+  }
 }
